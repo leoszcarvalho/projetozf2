e14a3335f7262bc9ca2144bdb4537323ae83ef6a
root <root@localhost.localdomain>
2015-02-16 22:09:33 -0200

    Commit simples identação

--- module/Album/Module.php
+++ module/Album/Module.php
@@ -25,15 +25,15 @@ class Module
         $moduleRouteListener->attach($eventManager);
         
         
-         $e->getApplication()->getEventManager()->getSharedManager()->attach('Zend\Mvc\Controller\AbstractController', 'dispatch', function($e) {
-$controller = $e->getTarget();
-$controllerClass = get_class($controller);
-$moduleNamespace = substr($controllerClass, 0, strpos($controllerClass, '\\'));
-$config = $e->getApplication()->getServiceManager()->get('config');
-if (isset($config['module_layouts'][$moduleNamespace])) {
-$controller->layout($config['module_layouts'][$moduleNamespace]);
-}
-}, 100);
+        $e->getApplication()->getEventManager()->getSharedManager()->attach('Zend\Mvc\Controller\AbstractController', 'dispatch', function($e) {
+        $controller = $e->getTarget();
+        $controllerClass = get_class($controller);
+        $moduleNamespace = substr($controllerClass, 0, strpos($controllerClass, '\\'));
+        $config = $e->getApplication()->getServiceManager()->get('config');
+        if (isset($config['module_layouts'][$moduleNamespace])) {
+        $controller->layout($config['module_layouts'][$moduleNamespace]);
+        }
+        }, 100);
         
     }
 
